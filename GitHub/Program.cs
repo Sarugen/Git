b479659304c063b57364f2c2be0528eb504cf5cd
Sarugen <sepenzev97@gmail.com>
2022-10-30 13:41:43 +0200

    Update Program.cs

--- GitHub/Program.cs
+++ GitHub/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string str = "abcdefg";
+Console.WriteLine("1) The length of '{0}' is {1}", str, str.Length);
+Console.WriteLine("2) The length of '{0}' is {1}", "xyz", "xyz".Length);
+
+int length = str.Length;
+Console.WriteLine("3) The length of '{0}' is {1}", str, length);
